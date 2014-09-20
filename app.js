5ab8f1e2f97bd72a6982a6091500cdf84994d092
Luke Williams <luke@iquidus.co.nz>
2014-09-20 16:08:11 +1200

    make heavy setting a local

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.set('markets', settings.markets);
 app.set('twitter', settings.twitter);
 app.set('genesis_block', settings.genesis_block);
 app.set('index', settings.index);
+app.set('heavy', settings.heavy);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
