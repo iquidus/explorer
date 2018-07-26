a66b28b1216ca90e1e42fd292f42c926c6661279
2g4y1 <0043664@gmail.com>
2018-07-26 12:22:35 +0200

    -

--- models/peers.js
+++ models/peers.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
 var PeersSchema = new Schema({
-  createdAt: { type: Date, expires: 86400, default: Date.now()},
+  createdAt: { type: Date, expires: 172800, default: Date.now()},
   address: { type: String, default: "" },
   protocol: { type: String, default: "" },
   version: { type: String, default: "" },
