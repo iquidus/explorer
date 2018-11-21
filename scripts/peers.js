721697248e14ac0b219d74f832693edc62dc8a19
cryptozeny <cryptozeny@gmail.com>
2018-11-21 19:43:24 +0900

    Sugarchain: fix Geo IP Network location
    get an ipstack account key:
    https://ipstack.com/documentation
    
    replace this string to yours:
    YourIpstackAPIKey

--- scripts/peers.js
+++ scripts/peers.js
@@ -32,7 +32,8 @@ mongoose.connect(dbString, function(err) {
             // peer already exists
             loop.next();
           } else {
-            request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
+            // request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
+            request({uri: 'http://api.ipstack.com/' + address +'?access_key=' + settings.peers.ipstack_api_key, json: true}, function (error, response, geo) {
               db.create_peer({
                 address: address,
                 protocol: body[i].version,
