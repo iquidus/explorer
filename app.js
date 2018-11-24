12d3ae9df72ba73d1907321fb16afc9870687845
cryptozeny <cryptozeny@gmail.com>
2018-11-25 01:25:47 +0900

    Sugarchain: cosmetic: add warning message

--- app.js
+++ app.js
@@ -127,6 +127,7 @@ app.set('show_sent_received', settings.show_sent_received);
 app.set('logo', settings.logo);
 app.set('theme', settings.theme);
 app.set('labels', settings.labels);
+app.set('movement', settings.movement);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
