cf4ac6d4db783e29443dfbd303dd6be95761c50d
cryptozeny <cryptozeny@gmail.com>
2018-11-25 04:01:13 +0900

    Sugarchain: movement alert apply to explorer default

--- routes/index.js
+++ routes/index.js
@@ -89,7 +89,7 @@ function route_get_tx(res, txid) {
 }
 
 function route_get_index(res, error) {
-  res.render('index', { active: 'home', error: error, warning: null});
+  res.render('index', { active: 'home', error: error, warning: null, flaga: settings.movement.low_flag, flagb: settings.movement.high_flag, min_amount:settings.movement.min_amount});
 }
 
 function route_get_address(res, hash, count) {
