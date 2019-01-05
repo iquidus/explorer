8582b98a452e545cc81e990dedc5c66004b69df9
Tim <timgarrity89@gmail.com>
2019-01-05 00:27:00 -0500

    Fixed up the masternodes page from diquidus for our usage.

--- scripts/masternodes.js
+++ scripts/masternodes.js
@@ -15,7 +15,7 @@ dbString = dbString + '@' + settings.dbsettings.address;
 dbString = dbString + ':' + settings.dbsettings.port;
 dbString = dbString + '/' + settings.dbsettings.database;
 
-mongoose.connect(dbString, { useNewUrlParser: true }, function (err) {
+mongoose.connect(dbString, { createIndexes: true, useNewUrlParser: true }, function (err) {
     if (err) {
         console.log('Unable to connect to database: %s', dbString);
         console.log('Aborting');
