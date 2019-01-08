005da532f4f06c7b006f96f2f65626ba383c76ef
Tim <timgarrity89@gmail.com>
2019-01-08 05:57:00 -0500

    Basic implementation of the get_mn/stake_rewards

--- app.js
+++ app.js
@@ -125,6 +125,16 @@ app.use('/ext/getbalance/:hash', function(req,res){
   });
 });
 
+app.use('/ext/get_masternode_rewards/:hash/:since', function(req, res){
+  db.get_masternode_rewards(req.params.since, req.params.hash,function(rewards){
+    if(rewards){
+      res.json(rewards);
+    } else {
+      res.send({error: "something wrong", hash: req.params.hash, since: req.params.since});
+    }
+  })
+})
+
 app.use('/ext/getdistribution', function(req,res){
   db.get_richlist(settings.coin, function(richlist){
     db.get_stats(settings.coin, function(stats){
