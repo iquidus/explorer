b159b0a142e169f8e7b780ee0b96ea9a76853613
Tim Garrity <timgarrity89@gmail.com>
2019-05-01 12:17:20 -0400

    #10 added checks for errors in peers.js

--- scripts/peers.js
+++ scripts/peers.js
@@ -19,7 +19,20 @@ dbString = dbString + ':' + settings.dbsettings.port;
 dbString = dbString + '/' + settings.dbsettings.database;
 
 function create_peers(address, protocol, version){
-  //request({uri: 'http://api.ipstack.com/' + address + '?access_key=' + settings.peers.apikey, json: true}, function (error, response, geo) {
+  if(settings.peers.use_IPStack){
+    request({uri: 'http://api.ipstack.com/' + address + '?access_key=' + settings.peers.apikey, json: true}, function (error, response, geo) {
+      db.create_peer({
+        address: address,
+        protocol: protocol,
+        version: version,
+        //todo
+        //semver: semver,
+        country: geo.country_name
+      }, function(){
+        console.log('added %s to the peers database', address)
+      });
+    });
+  }else{
     db.create_peer({
       address: address,
       protocol: protocol,
@@ -28,8 +41,9 @@ function create_peers(address, protocol, version){
       //semver: semver,
       country: "geo.country_name"
     }, function(){
+      console.log('added %s to the peers database', address)
     });
-  //});
+  }
 }
 
 mongoose.connect(dbString, { useCreateIndex: true,
@@ -41,59 +55,63 @@ mongoose.connect(dbString, { useCreateIndex: true,
     exit();
   } else {
     request({uri: 'http://'+settings.address +':' + settings.port + '/api/getpeerinfo', json: true}, function (error, response, body) {
-      var livepeers = [];
-      if(settings.peers.purge_on_run) {
-        db.purge_peers();
-      }
-      lib.syncLoop(body.length, function (loop) {
-        var i = loop.iteration();
-        //https://github.com/suprnurd/ciquidus/pull/11/commits/61dec88f1b660aabdfdebef6fb3068186eb047de
-        var address = body[i].addr
-        if (body[i].addr.indexOf('[') == 0 && body[i].addr.indexOf(']') > -1) {
-          address = address.slice(1, address.indexOf(']'));
-        }else{
-          address = address.split(':')[0];
+      if(error != null){
+        console.log(error);
+      }else{
+        var livepeers = [];
+        if(settings.peers.purge_on_run) {
+          db.purge_peers();
         }
-        //end
-        var version = body[i].subver.replace('/', '').replace('/', '');
-        var semver = version.split(":")[1];
-        livepeers[i] = address;
-        db.find_peers(address, function(peer) {
-          if (peer.length) {
-              for(i=0; i<peer.length; i++){
-                // cmp(a,b)
-                // result 1 = a is greater than b
-                // result 0 = a is the same as b
-                // result -1 = a is less than b
-                if(cmp(peer[i].version.split(":")[1], semver) == -1){
-                  if(settings.peers.purge_on_run != true){
-                    db.delete_peer({_id:peer[i]._id});
+        lib.syncLoop(body.length, function (loop) {
+          var i = loop.iteration();
+          //https://github.com/suprnurd/ciquidus/pull/11/commits/61dec88f1b660aabdfdebef6fb3068186eb047de
+          var address = body[i].addr
+          if (body[i].addr.indexOf('[') == 0 && body[i].addr.indexOf(']') > -1) {
+            address = address.slice(1, address.indexOf(']'));
+          }else{
+            address = address.split(':')[0];
+          }
+          //end
+          var version = body[i].subver.replace('/', '').replace('/', '');
+          var semver = version.split(":")[1];
+          livepeers[i] = address;
+          db.find_peers(address, function(peer) {
+            if (peer.length) {
+                for(i=0; i<peer.length; i++){
+                  // cmp(a,b)
+                  // result 1 = a is greater than b
+                  // result 0 = a is the same as b
+                  // result -1 = a is less than b
+                  if(cmp(peer[i].version.split(":")[1], semver) == -1){
+                    if(settings.peers.purge_on_run != true){
+                      db.delete_peer({_id:peer[i]._id});
+                    }
+                    create_peers(address, body[i].version, version);
+                    console.log('Delete the db version:', peer[i].version.split(":")[1]); //remove
+                  } else if(cmp(peer[i].version.split(":")[1], semver) == 0){
+                    console.log('Do nothing, they\'re the same');
+                  } else {
+                    //db.delete_peer({_id:peer[i]._id});
+                    console.log('This should never occur, Live Version:', semver, " Is less than:", peer[i].version.split(":")[1]); //remove
                   }
-                  create_peers(address, body[i].version, version);
-                  //console.log('Delete the db version:', peer[i].version.split(":")[1]); //remove
-                } else if(cmp(peer[i].version.split(":")[1], semver) == 0){
-                    //console.log('Do nothing, they\'re the same');
-                } else {
-                  //db.delete_peer({_id:peer[i]._id});
-                  console.log('This should never occur, Live Version:', semver, " Is less than:", peer[i].version.split(":")[1]); //remove
                 }
-              }
+                loop.next();
+            } else {
+              create_peers(address, body[i].version, version);            
               loop.next();
-          } else {
-            create_peers(address, body[i].version, version);            
-            loop.next();
-          }
-        });
-      },function(){
-        db.get_peers(function(peers){
-          for( var i = 0; i < peers.length; i++){
-            if(!livepeers.includes(peers[i].address)){
-              db.delete_peer({address:peers[i].address});
             }
-          }
-		      exit();
+          });
+        },function(){
+          db.get_peers(function(peers){
+            for( var i = 0; i < peers.length; i++){
+              if(!livepeers.includes(peers[i].address)){
+                db.delete_peer({address:peers[i].address});
+              }
+            }
+            exit();
+          });
         });
-      });
+      }
     });
   };
 });
