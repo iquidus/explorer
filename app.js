170635bfbf91d12d39fb374f70865d2cc2300bc0
Unknown <theholyroger@theholyroger.com>
2019-12-21 13:04:18 +0000

    Clean up deprecated address.txs fields

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ app.use('/ext/getaddress/:hash', function(req,res){
         sent: (address.sent / 100000000),
         received: (address.received / 100000000),
         balance: (address.balance / 100000000).toString().replace(/(^-+)/mg, ''),
-        last_txs: address.txs,
       };
       res.send(a_ext);
     } else {
