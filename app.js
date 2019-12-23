976db88799abd61d2a19fec1ce93e05545c09bed
Unknown <theholyroger@theholyroger.com>
2019-12-23 22:13:50 +0000

    Updated BodyParser - deprecated warning fix

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.set('view engine', 'jade');
 app.use(favicon(path.join(__dirname, settings.favicon)));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
