a57e734a6887697821fe1c34d8268776bab03d8c
Unknown <theholyroger@theholyroger.com>
2019-12-24 11:30:42 +0000

    Movement: Use new lasttxsajax call and remove old code

--- app.js
+++ app.js
@@ -143,20 +143,19 @@ app.use('/ext/getdistribution', function(req,res){
   });
 });
 
-app.use('/ext/getlasttxs/:min', function(req,res){
-  db.get_last_txs(settings.index.last_txs, (req.params.min * 100000000), function(txs){
-    res.send({data: txs});
-  });
-});
-
-app.use('/ext/getlasttxsajax', function(req,res){
+app.use('/ext/getlasttxsajax/:min', function(req,res){
   if(typeof req.query.length === 'undefined' || isNaN(req.query.length) || req.query.length > settings.index.last_txs){
     req.query.length = settings.index.last_txs;
   }
   if(typeof req.query.start === 'undefined' || isNaN(req.query.start) || req.query.start < 0){
-      req.query.start = 0;
+    req.query.start = 0;
   }
-  db.get_last_txs_ajax(req.query.start, req.query.length,function(txs, count){
+  if(typeof req.params.min === 'undefined' || isNaN(req.params.min ) || req.params.min  < 0){
+    req.params.min  = 0;
+  } else {
+    req.params.min  = (req.params.min * 100000000);
+  }
+  db.get_last_txs_ajax(req.query.start, req.query.length, req.params.min,function(txs, count){
     var data = [];
     for(i=0; i<txs.length; i++){
       var row = [];
