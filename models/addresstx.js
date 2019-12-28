739a6f9c994e277b4bae6bda61e32db7df35407c
Unknown <theholyroger@theholyroger.com>
2019-12-28 13:20:57 +0000

    Address TX balance indexing fix

--- models/addresstx.js
+++ models/addresstx.js
@@ -5,6 +5,7 @@ var AddressTXSchema = new Schema({
   a_id: { type: String, index: true},
   blockindex: {type: Number, default: 0, index: true},
   txid: { type: String, lowercase: true, index: true},
+  // renamed to amount instead of balance for future changes
   amount: { type: Number, default: 0}
 }, {id: false});
 
