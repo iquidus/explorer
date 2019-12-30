c0fd4448682623cde57f46ba6ea1cace3a46861b
Unknown <theholyroger@theholyroger.com>
2019-12-30 10:46:46 +0000

    Add version to footer

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express = require('express')
   , routes = require('./routes/index')
   , lib = require('./lib/explorer')
   , db = require('./lib/database')
+  , package_metadata = require('./package.json')
   , locale = require('./lib/locale')
   , request = require('request');
 
@@ -220,6 +221,7 @@ app.use('/ext/connections', function(req,res){
 
 // locals
 app.set('title', settings.title);
+app.set('iquidus_version', package_metadata.version);
 app.set('symbol', settings.symbol);
 app.set('coin', settings.coin);
 app.set('locale', locale);
